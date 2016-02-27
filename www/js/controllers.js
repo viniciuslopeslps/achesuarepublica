47b1519ac4ea3a8df83cc7bbb6b6aa8423525797
Vinicius <viniciuslopeslps@gmail.com>
2016-02-27 16:48:37 -0300

    preparando ambiente

---
+++ www/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+})
+
+.controller('createUserCtrl', function($scope, $stateParams) {
+
+})
+;
