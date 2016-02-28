2c54f8459bc97486b42dd9c1a4753a46e44e31c5
Vinicius <viniciuslopeslps@gmail.com>
2016-02-28 10:25:26 -0300

    pegando dados do form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,13 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('createUserCtrl', function($scope, $stateParams) {
-
+.controller('createUserCtrl', function($scope, $state, $ionicPopup, $http, $ionicLoading) {
+    $scope.createUser = function(data) {
+      var email = $scope.data.email;
+      var name = $scope.data.name;
+      var password = $scope.data.password;
+      var phone = $scope.data.phone;
+      console.log(email + name + password + phone);
+    };
 })
 ;
